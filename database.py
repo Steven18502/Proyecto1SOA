f5f568f0d14241c1b8bdbe0766f3383cc33888b2
Shakime Sparks <jeykimers.johnson@gmail.com>
2022-09-17 05:24:32 -0600

    ADD database connection

---
+++ database.py
@@ -0,0 +1,15 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="104.197.57.229",
+  user="root",
+  password="123456",
+  database="Proyecto1"
+)
+mycursor = mydb.cursor()
+sql = "insert into resultados (face, joy, sorrow,anger,surprise) VALUES (%s, %s,%s, %s,%s)"
+
+def insert(val):
+  val = (val[0],val[1],val[2],val[3],val[4])
+  mycursor.execute(sql, val)
+  mydb.commit()
+  print("Register inserted!!")
--- terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-
-variable "project_id" {
-    default = "soa-project1-361306"
-}
-
-variable "region" {
-    default = "europe-west1"
-}
